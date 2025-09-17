c97af5b101010e4e390098f1007f6a013d5e2a4a
luxifearzz <luxifearzz@gmail.com>
2025-09-17 15:47:31 +0700

    work(add): cellule1.0-1.3

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Wachirawit"
+last_name = "Jeamoery"
+
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Wachirawit"
+last_name = "Jeamoery"
+whole_name = first_name + " " + last_name
+
+print(whole_name)
