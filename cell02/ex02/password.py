9a787f4224182e0c4116051ea89ae1cf93539063
luxifearzz <luxifearzz@gmail.com>
2025-09-17 15:54:40 +0700

    work(add): cellule2.0-2.3

---
+++ cell02/ex02/password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+password = "Python is awesome"
+entered_password = input()
+
+if (password == entered_password):
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
